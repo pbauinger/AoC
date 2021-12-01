81af1a654785289df57004a736e62a59a02fe189
Philipp Bauinger <philipp.bauinger@gmail.com>
2021-12-01 06:36:32 +0100

    added 2021/1

---
+++ 2021/01/day1.py
@@ -0,0 +1,31 @@
+import re
+import itertools as it
+import more_itertools as mit
+import numpy as np
+from collections import deque, defaultdict
+from functools import cache
+from pathlib import Path
+from math import prod
+
+def part1(arr):
+    prev = arr[0]
+    cnt = 0
+    for curr in arr[1:]:
+        if curr > prev:
+            cnt += 1
+        prev = curr
+    return cnt
+
+def part2(arr):
+    prev = float("-inf")
+    cnt = -1
+    for curr in mit.windowed(arr, 3):
+        if sum(curr) > prev:
+            cnt += 1
+        prev = sum(curr)
+    return cnt
+
+input = Path("real.in").read_text()
+input = [int(x) for x in input.splitlines()]
+print(part1(input))
+print(part2(input))
