2441eacc74e28e5812e6992816d479af4afc8f10
Philipp Bauinger <philipp.bauinger@gmail.com>
2021-12-06 09:33:23 +0100

    added day6

---
+++ 2021/06/day6.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from functools import cache
+
+
+@cache
+def calcFish(fish, curr, remaining):
+    total = 1
+    for day in range(curr + fish, remaining + 1, 7):
+        total += calcFish(9, day, remaining)
+    return total
+
+
+def calc(days):
+    data = [int(x) for x in Path("real.in").read_text().split(",")]
+    total = 0
+    for fish in data:
+        total += calcFish(fish, 1, days)
+    return total
+
+
+print("Part 1", calc(80))
+print("Part 2", calc(256))
