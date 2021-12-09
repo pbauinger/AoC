69ed8b6700eef4a1cb39e63f7a177445cd7b9d6b
Philipp Bauinger <philipp.bauinger@gmail.com>
2021-12-09 15:13:12 +0100

    refactorings

--- 2021/09/day9.py
+++ 2021/09/day9.py
@@ -2,18 +2,15 @@ from pathlib import Path
 from math import prod
 
 
-def expand(row, col):
-    curr = hmap[row][col]
-    positions = {(row, col)}
-    if row != 0 and hmap[row-1][col] != 9 and hmap[row-1][col] > curr:
-        positions |= expand(row - 1, col)
-    if row != len(hmap)-1 and hmap[row+1][col] != 9 and hmap[row+1][col] > curr:
-        positions |= expand(row + 1, col)
-    if col != 0 and hmap[row][col-1] != 9 and hmap[row][col-1] > curr:
-        positions |= expand(row, col - 1)
-    if col != len(hmap[0])-1 and hmap[row][col+1] != 9 and hmap[row][col+1] > curr:
-        positions |= expand(row, col + 1)
-    return positions
+def expand(row, col, prev=float("-inf")):
+    if (row < 0 or row >= len(hmap) or col < 0 or col >= len(hmap[0])
+            or hmap[row][col] == 9 or hmap[row][col] <= prev):
+        return set()
+    return ({(row, col)}
+            | expand(row - 1, col, hmap[row][col])
+            | expand(row + 1, col, hmap[row][col])
+            | expand(row, col - 1, hmap[row][col])
+            | expand(row, col + 1, hmap[row][col]))
 
 
 data = Path("real.in").read_text().splitlines()
@@ -27,10 +24,10 @@ low_corr = []
 for row in range(len(hmap)):
     for col in range(len(hmap[0])):
         curr = hmap[row][col]
-        low = row == 0 or curr < hmap[row-1][col]
-        low &= row == len(hmap)-1 or curr < hmap[row+1][col]
-        low &= col == 0 or curr < hmap[row][col-1]
-        low &= col == len(hmap[0])-1 or curr < hmap[row][col+1]
+        low = ((row == 0 or curr < hmap[row-1][col])
+               and (row == len(hmap)-1 or curr < hmap[row+1][col])
+               and (col == 0 or curr < hmap[row][col-1])
+               and (col == len(hmap[0])-1 or curr < hmap[row][col+1]))
         if low:
             low_corr.append((row, col))
             low_points.append(curr)
